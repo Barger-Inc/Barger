080e1868b6b7d70abec77b046f630917d9b2f637
Виталий <46228376+LayXL@users.noreply.github.com>
2024-10-14 22:04:34 +0500

    Improvements

--- apps/admin-panel-frontend/src/app/(admin)/page.tsx
+++ apps/admin-panel-frontend/src/app/(admin)/page.tsx
@@ -2,15 +2,29 @@ import { Button } from "@/shared/ui/button"
 
 export default function Page() {
   return (
-    <div>
+    <div className="p-4">
       <h1>Home Page</h1>
-      <Button
-        leadingIcon={{
-          name: "attach",
-          color: "black",
-        }}
-        label="Click me"
-      />
+
+      <div className="flex gap-2">
+        <Button
+          leadingIcon={{
+            name: "attach",
+            color: "black",
+          }}
+          trailingIcon={{
+            name: "attach",
+            color: "black",
+          }}
+          label="Click me"
+        />
+
+        <Button
+          icon={{
+            name: "attach",
+            color: "black",
+          }}
+        />
+      </div>
     </div>
   )
 }
--- apps/admin-panel-frontend/src/shared/ui/button.tsx
+++ apps/admin-panel-frontend/src/shared/ui/button.tsx
@@ -1,29 +1,51 @@
-import { type ComponentPropsWithoutRef, forwardRef } from "react"
+import {
+  type ComponentPropsWithoutRef,
+  type ReactNode,
+  forwardRef,
+} from "react"
 import { Icon, type IconProps } from "./icon"
 import { PrimitiveButton } from "./privitive-button"
 
 type ButtonProps = {
+  before?: ReactNode
+  after?: ReactNode
   leadingIcon?: IconProps
   trailingIcon?: IconProps
+  icon?: IconProps
   label?: string
 } & ComponentPropsWithoutRef<typeof PrimitiveButton>
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props) => {
-  const { leadingIcon, trailingIcon, label, ...otherProps } = props
+  const {
+    before,
+    after,
+    leadingIcon,
+    trailingIcon,
+    icon,
+    label,
+    ...otherProps
+  } = props
 
   return (
     <PrimitiveButton {...otherProps}>
+      {before}
       {leadingIcon && (
-        <span className="pr-2">
+        <span className="mr-2">
           <Icon size={16} {...leadingIcon} />
         </span>
       )}
-      <span>{label}</span>
+      {icon && (
+        <span>
+          <Icon size={16} {...icon} />
+        </span>
+      )}
+      <span className="text-center w-full">{label}</span>
       {trailingIcon && (
-        <span className="ml-auto pr-2">
+        <span className="ml-2">
           <Icon size={16} {...trailingIcon} />
         </span>
       )}
+      {after}
     </PrimitiveButton>
   )
 })
