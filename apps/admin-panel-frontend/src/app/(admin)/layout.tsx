5904b9cee659f95860c15217d5e0b731b94bd29f
Виталий <46228376+LayXL@users.noreply.github.com>
2024-10-14 14:35:54 +0500

    Disable spinner for top loader

--- apps/admin-panel-frontend/src/app/(admin)/layout.tsx
+++ apps/admin-panel-frontend/src/app/(admin)/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
         ]}
       />
       <main>
-        <NextTopLoader />
+        <NextTopLoader showSpinner={false} />
         {children}
       </main>
     </div>
