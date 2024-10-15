23b79fddbd0e41b2df442d489acbbdf4418d102b
Виталий <46228376+LayXL@users.noreply.github.com>
2024-10-15 09:59:34 +0500

    ref fixes

--- apps/admin-panel-frontend/src/shared/ui/button.tsx
+++ apps/admin-panel-frontend/src/shared/ui/button.tsx
@@ -26,53 +26,56 @@ type ButtonProps = {
 ) &
   ComponentPropsWithoutRef<typeof PrimitiveButton>
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props) => {
-  const {
-    before,
-    after,
-    leadingIcon,
-    trailingIcon,
-    icon,
-    label,
-    ...otherProps
-  } = props
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const {
+      before,
+      after,
+      leadingIcon,
+      trailingIcon,
+      icon,
+      label,
+      ...otherProps
+    } = props
 
-  const memoizedLeadingIcon = useMemo(
-    () =>
-      leadingIcon && (
-        <span className="mr-2">
-          <Icon {...getIconProps(leadingIcon, { size: 16 })} />
-        </span>
-      ),
-    [leadingIcon]
-  )
+    const memoizedLeadingIcon = useMemo(
+      () =>
+        leadingIcon && (
+          <span className="mr-2">
+            <Icon {...getIconProps(leadingIcon, { size: 16 })} />
+          </span>
+        ),
+      [leadingIcon]
+    )
 
-  const memoizedIcon = useMemo(
-    () => icon && <Icon {...getIconProps(icon, { size: 16 })} />,
-    [icon]
-  )
+    const memoizedIcon = useMemo(
+      () => icon && <Icon {...getIconProps(icon, { size: 16 })} />,
+      [icon]
+    )
 
-  const memoizedTrailingIcon = useMemo(
-    () =>
-      trailingIcon && (
-        <span className="ml-2">
-          <Icon {...getIconProps(trailingIcon, { size: 16 })} />
-        </span>
-      ),
-    [trailingIcon]
-  )
+    const memoizedTrailingIcon = useMemo(
+      () =>
+        trailingIcon && (
+          <span className="ml-2">
+            <Icon {...getIconProps(trailingIcon, { size: 16 })} />
+          </span>
+        ),
+      [trailingIcon]
+    )
 
-  return (
-    <PrimitiveButton
-      {...otherProps}
-      className={cn(props.icon && "w-9 px-0", otherProps.className)}
-    >
-      {before}
-      {memoizedLeadingIcon}
-      {memoizedIcon}
-      {label && <span className="text-center w-full">{label}</span>}
-      {memoizedTrailingIcon}
-      {after}
-    </PrimitiveButton>
-  )
-})
+    return (
+      <PrimitiveButton
+        {...otherProps}
+        ref={ref}
+        className={cn(props.icon && "w-9 px-0", otherProps.className)}
+      >
+        {before}
+        {memoizedLeadingIcon}
+        {memoizedIcon}
+        {label && <span className="text-center w-full">{label}</span>}
+        {memoizedTrailingIcon}
+        {after}
+      </PrimitiveButton>
+    )
+  }
+)
