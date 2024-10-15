d019847ebcfb2005c7db5e272c353ce901bdb76e
Виталий <46228376+LayXL@users.noreply.github.com>
2024-10-15 09:38:35 +0500

    cache control and icons

--- apps/admin-panel-frontend/next.config.mjs
+++ apps/admin-panel-frontend/next.config.mjs
@@ -9,6 +9,20 @@ const nextConfig = {
       },
     ]
   },
+  headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
