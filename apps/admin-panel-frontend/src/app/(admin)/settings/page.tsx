2a3bd15e887cecbb9d9c771791bbc916a2ba9bcb
Виталий <46228376+LayXL@users.noreply.github.com>
2024-11-04 20:14:26 +0500

    removed props from settings page

--- apps/admin-panel-frontend/src/app/(admin)/settings/page.tsx
+++ apps/admin-panel-frontend/src/app/(admin)/settings/page.tsx
@@ -1,12 +1,6 @@
 import { LanguageSelector } from "@/widgets/language-selector"
 
-type Props = {
-  params: {
-    locale: string
-  }
-}
-
-export default function Page({ params: { locale } }: Props) {
+export default function Page() {
   return (
     <div>
       Settings Page
