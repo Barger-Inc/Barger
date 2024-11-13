1dd49cf7bbb47ddf3b58cd6ce0ed4c46fd6a1152
Виталий <46228376+LayXL@users.noreply.github.com>
2024-11-13 21:26:24 +0500

    Improvements in change password

--- apps/admin-panel-frontend/src/app/(admin)/profile/changePassword/page.tsx
+++ apps/admin-panel-frontend/src/app/(admin)/profile/changePassword/page.tsx
@@ -1,17 +1,10 @@
 "use client"
-
-import { Button } from "@/shared/ui/button"
 import { ChangePassword } from "@/widgets/change-password"
-import { useTranslations } from "next-intl"
 
 export default function Page() {
-  // TODO: это не модалка, поэтому changePasswordModal не уместно
-  const t = useTranslations("changePasswordModal")
-
   return (
-    <div>
+    <div className={"max-w-[480px]"}>
       <ChangePassword />
-      <Button label={t("save")} />
     </div>
   )
 }
--- apps/admin-panel-frontend/src/shared/ui/password-input.tsx
+++ apps/admin-panel-frontend/src/shared/ui/password-input.tsx
@@ -1,38 +1,51 @@
 "use client"
 
+import withDefaultProps from "@/shared/hoc/with-default-props"
 import { TextField } from "@/shared/ui/text-field"
 import { TextField as PrimitiveTextField } from "@radix-ui/themes"
 import { IconButton } from "@radix-ui/themes"
-import { forwardRef, useState } from "react"
+import { Text } from "@radix-ui/themes"
+import { type ComponentPropsWithoutRef, forwardRef, useState } from "react"
 import { Icon } from "./icon"
+
+const ErrorCaption = withDefaultProps(Text, {
+  color: "red",
+  size: "1",
+})
 
 type PasswordInputProps = {
   label?: string
-}
+  errorMessage?: string
+} & ComponentPropsWithoutRef<typeof PrimitiveTextField.Root>
 
 export const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
   (props, ref) => {
-    const [isVisible, setIsVisible] = useState(false)
+    const { label, ...otherProps } = props
 
+    // TODO: make hook useSwitch or smth
+    const [isVisible, setIsVisible] = useState(false)
     const toggleVisibility = () => setIsVisible((prev) => !prev)
 
     return (
-      <TextField
-        ref={ref}
-        label={props.label}
-        size={"2"}
-        type={isVisible ? "text" : "password"}
-      >
-        <PrimitiveTextField.Slot side="right">
-          <IconButton variant="ghost" color="gray" onClick={toggleVisibility}>
-            <Icon
-              size={16}
-              name={isVisible ? "eye-open" : "eye-closed"}
-              variant={"stroke"}
-            />
-          </IconButton>
-        </PrimitiveTextField.Slot>
-      </TextField>
+      <div className="flex flex-col gap-1">
+        <TextField
+          ref={ref}
+          label={label}
+          type={isVisible ? "text" : "password"}
+          {...otherProps}
+        >
+          <PrimitiveTextField.Slot side="right">
+            <IconButton variant="ghost" color="gray" onClick={toggleVisibility}>
+              <Icon
+                size={16}
+                name={isVisible ? "eye-open" : "eye-closed"}
+                variant={"stroke"}
+              />
+            </IconButton>
+          </PrimitiveTextField.Slot>
+        </TextField>
+        {props.errorMessage && <ErrorCaption children={props.errorMessage} />}
+      </div>
     )
   }
 )
