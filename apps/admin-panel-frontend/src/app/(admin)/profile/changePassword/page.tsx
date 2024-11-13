b0b9e45ead81280eeae16f1dc8f661f4c6cc6fa4
Виталий <46228376+LayXL@users.noreply.github.com>
2024-11-13 20:12:21 +0500

    Improvements

--- apps/admin-panel-frontend/src/app/(admin)/profile/changePassword/page.tsx
+++ apps/admin-panel-frontend/src/app/(admin)/profile/changePassword/page.tsx
@@ -1,38 +1,17 @@
 "use client"
 
-import { ChangePassword } from "@/widgets/change-password"
-import { useState } from "react"
 import { Button } from "@/shared/ui/button"
+import { ChangePassword } from "@/widgets/change-password"
 import { useTranslations } from "next-intl"
-import { useRouter } from "next/navigation"
 
 export default function Page() {
-  const t = useTranslations("change-password-modal")
-  const [currentPassword, setcurrentPassword] = useState<string | null>(null)
-  const [newPassword, setnewPassword] = useState<string | null>(null)
-  const [passwordAgain, setpasswordAgain] = useState<string | null>(null)
-
-  const router = useRouter()
-
-  const routefun = () => {
-    router.back()
-  }
+  // TODO: это не модалка, поэтому changePasswordModal не уместно
+  const t = useTranslations("changePasswordModal")
 
   return (
     <div>
-      <ChangePassword
-        onUpdate={(currentPassword, newPassword, passwordAgain) => {
-          setcurrentPassword(currentPassword)
-          setnewPassword(newPassword)
-          setpasswordAgain(passwordAgain)
-        }}
-      />
-      <Button
-        label={t("save")}
-        onClick={() => {
-          routefun()
-        }}
-      />
+      <ChangePassword />
+      <Button label={t("save")} />
     </div>
   )
 }
