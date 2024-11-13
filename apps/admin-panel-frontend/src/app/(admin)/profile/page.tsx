ad3f37f5bf55a212c0a26fb054b12a74d39d292e
Виталий <46228376+LayXL@users.noreply.github.com>
2024-11-13 21:56:28 +0500

    layout fixes

--- apps/admin-panel-frontend/src/app/(admin)/profile/page.tsx
+++ apps/admin-panel-frontend/src/app/(admin)/profile/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
       <div className="flex flex-col gap-6">
         <Heading size="5" weight="medium" children={t("security")} />
 
-        <div className="flex flex-col gap-2 sm:flex-row sm:[&>*]:flex-1 sm:[&>*>*]:w-full">
+        <div className="flex flex-col gap-2 sm:flex-row sm:[&>*]:flex-1 [&>*>*]:w-full">
           <SoftButton label={t("changeEmail")} />
           <Link href={"/profile/changePassword"}>
             <SoftButton label={t("changePassword")} />
