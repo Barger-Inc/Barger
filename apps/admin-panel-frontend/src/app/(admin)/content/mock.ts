2785c9c10e7d5dbfc2e1a83a91ad27842e28c7a0
Виталий <46228376+LayXL@users.noreply.github.com>
2024-11-30 15:08:59 +0500

    new collections in mock file

--- apps/admin-panel-frontend/src/app/(admin)/content/mock.ts
+++ apps/admin-panel-frontend/src/app/(admin)/content/mock.ts
@@ -1,11 +1,10 @@
 // TODO: move to other folder
 export const mockCollections = [
-  {
-    id: 1,
-    title: "Новости",
-  },
-  {
-    id: 2,
-    title: "Объявления",
-  },
+  { id: 1, title: "Новости" },
+  { id: 2, title: "Объявления" },
+  { id: 3, title: "Товары" },
+  { id: 4, title: "Пользователи" },
+  { id: 5, title: "Мероприятия" },
+  { id: 6, title: "Статьи" },
+  { id: 7, title: "Кейсы" },
 ] as const
