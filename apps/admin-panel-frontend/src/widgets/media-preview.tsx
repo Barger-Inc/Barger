d863c1ae6a09f3e938403b3f9f0ff08768250fb8
Kustov-Artyom <artemkust174@gmail.com>
2024-12-04 02:00:24 +0500

    media update

--- apps/admin-panel-frontend/src/widgets/media-preview.tsx
+++ apps/admin-panel-frontend/src/widgets/media-preview.tsx
@@ -2,8 +2,15 @@ import { useTransform } from "framer-motion"
 import { TextField } from "../shared/ui/text-field"
 import { Badge, Separator, Text } from "@radix-ui/themes"
 import { useTranslations } from "next-intl"
+import { format } from "date-fns/format"
 type MediaPreviewProps = {
   id: number
+  width: number
+  height: number
+  extension: string
+  size: number
+  date: Date
+  url: string
 }
 
 export const MediaPreview = (props: MediaPreviewProps) => {
@@ -14,7 +21,7 @@ export const MediaPreview = (props: MediaPreviewProps) => {
       <div className="overflow-hidden">
         <img
           className="w-full max-h-84 object-cover"
-          src="/mediaImg/loadMediaPreview.png"
+          src={props.url}
           alt=""
         />
       </div>
@@ -24,19 +31,19 @@ export const MediaPreview = (props: MediaPreviewProps) => {
         <div className="grid sm:grid-cols-2 gap-x-6 gap-y-4">
           <div className="flex justify-between items-center">
             <Text size="2" weight="medium" children={t("resolution")} />
-            <Badge>1920x1080</Badge>
+            <Badge>{props.width}x{props.height}</Badge>
           </div>
           <div className="flex justify-between items-center">
             <Text size="2" weight="medium" children="Вид" />
-            <Badge className="uppercase">jpeg</Badge>
+            <Badge className="uppercase">{props.extension}</Badge>
           </div>
           <div className="flex justify-between items-center">
             <Text size="2" weight="medium" children="Размер" />
-            <Badge>500 кб</Badge>
+            <Badge>{props.size} кб</Badge>
           </div>
           <div className="flex justify-between items-center">
             <Text size="2" weight="medium" children="Дата" />
-            <Badge>27/10/2024</Badge>
+            <Badge>{format(props.date, "dd.MM.yyyy")}</Badge>
           </div>
         </div>
       </div>
